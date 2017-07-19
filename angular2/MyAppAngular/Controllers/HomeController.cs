36399e79bd3024732ebd65bbabf8aabc9f5ca7f9
areliez <areliez.v25@gmail.com>
2017-07-18 23:28:15 -0400

    Create structure of the project

---
+++ angular2/MyAppAngular/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppAngular.Controllers
+{
+    public class HomeController: Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
